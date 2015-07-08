7526bcf3229171e181689d6f13ed68a94bb4d723
David Frahm <david@midmoapps.com>
2015-07-07 19:47:31 -0500

    Adds date time stamp to log filename to keep history, since we are auto-upgrading major things like Node, Cordova, Ionic

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 
 # Minimal script to bootstrap the main setup script
 
+NOW=`date '+%Y%m%d-%H%M%S'`
+LOGFILE="~/setup-$NOW.log"
+echo "View log in $LOGFILE"
 bin=$(dirname -- "$0")
-$bin/setup.sh 2>&1 | tee ~/setup.log
+$bin/setup.sh 2>&1 | tee ~/setup-$NOW.log
